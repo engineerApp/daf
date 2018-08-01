e11d6f4e17a22eda0e193deaa38338a8845d232f
amine chraibi <achraibi@ymagis.com>
2018-08-01 16:23:06 +0100

    improvement

--- src/main/java/com/ymagis/daf/App.java
+++ src/main/java/com/ymagis/daf/App.java
@@ -1,9 +1,11 @@
 package com.ymagis.daf;
 
+import java.util.HashSet;
+
 import org.apache.commons.lang3.StringUtils;
 
-import com.ymagis.daf.game.ApiGame;
 import com.ymagis.daf.game.Game;
+import com.ymagis.daf.game.LocalGame;
 import com.ymagis.daf.response.StartResponse;
 import com.ymagis.daf.response.TestResponse;
 
@@ -16,7 +18,7 @@ public class App {
 	}
 
 	public static void main(String[] args) {
-		Game game = new ApiGame();
+		Game game = new LocalGame("1549542788878738738767727272600000");
 		App app = new App(game);
 		System.out.println(app.startGame());
 		System.out.println("call count : " + game.getCallCount());
@@ -24,7 +26,7 @@ public class App {
 
 	public String startGame() {
 		StartResponse startResponse = game.start();
-		if (startResponse==null) {
+		if (startResponse == null) {
 			System.out.println("Can't call api");
 			System.exit(0);
 		}
@@ -44,10 +46,13 @@ public class App {
 		for (int i = 0; i < 10; i++) {
 			number = StringUtils.repeat(i + "", size);
 			int good = checkTest(number);
+			if (good == size) {
+				return number;
+			}
 			if (good > 0) {
 				for (int j = 0; j < good; j++) {
-					ret += i;
 					count++;
+					ret += i;
 				}
 			}
 			if (count == size) {
@@ -68,28 +73,68 @@ public class App {
 		return good;
 	}
 
-	public String phase2(String tableNumber) {
-		String string = StringUtils.repeat(tableNumber.charAt(0), size);
+	public Character[] stringToSet(String s) {
+		HashSet<Character> characters = new HashSet<Character>();
+		for (int i = 0; i < s.length(); i++) {
+			characters.add(s.charAt(i));
+		}
+		return characters.toArray(new Character[characters.size()]);
+	}
+
+	private boolean isIsomorph(String string) {
+		String s = "";
+		for (int i = 0; i < 10; i++) {
+			s = StringUtils.repeat(i + "", size);
+			if (s.equals(string)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public String phase2(String numberList) {
+		String string = StringUtils.repeat(numberList.charAt(0), size);
 		StringBuilder trouve = new StringBuilder(string);
-		StringBuilder p = new StringBuilder(string);
+		StringBuilder p;
+		Character[] characters = stringToSet(numberList);
 		for (int c = 0; c < size; ++c) {
+			System.out.println("----------------case " + c + "----------------");
+			p = new StringBuilder(string);
 			int noteMax = 0;
-			for (int n = 0; n < size; ++n) {
-				p.setCharAt(c, tableNumber.charAt(n));
+			for (int n = 0; n < characters.length; ++n) {
+				p.setCharAt(c, characters[n]);
+				if (isIsomorph(p.toString())) {
+					continue;
+				}
+				System.out.println(c + " " + n + " " + p.toString());
 				int note = checkTest(p.toString());
 				if (note == size) {
 					return p.toString();
 				}
 				if ((note > 0) && (note >= noteMax)) {
 					noteMax = note;
-					trouve.setCharAt(c, tableNumber.charAt(n));
+					trouve.setCharAt(c, characters[n]);
+					System.out.println(c + " " + n + " trouve : " + trouve + " noteMax : " + noteMax);
 				}
 			}
+			numberList = removeOne(trouve.charAt(c), numberList);
+			characters = stringToSet(numberList);
 		}
 		int noteFinal = checkTest(trouve.toString());
 		if (noteFinal == size) {
 			return trouve.toString();
 		}
 		return "not found best result is : " + trouve.toString();
+	}
+
+	private String removeOne(char c, String numberList) {
+		StringBuilder stringBuilder = new StringBuilder(numberList);
+		for (int i = 0; i < numberList.length(); i++) {
+			if (c == numberList.charAt(i)) {
+				stringBuilder.deleteCharAt(i);
+				return stringBuilder.toString();
+			}
+		}
+		return null;
 	}
 }
